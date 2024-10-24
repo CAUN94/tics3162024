d8d4447e610d263c9398b400ad0f6e17ebf0148f
caun94 <cristobalugarte6@gmail.com>
2024-10-24 17:42:37 -0300

    clase 10 login sencill

---
+++ clase10/clase10-jwt/src/app/layout.js
@@ -0,0 +1,16 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>
+          {process.env.title}
+        </title>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.2/dist/tailwind.min.css"
+          rel="stylesheet" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ clase10/clase10-jwt/src/app/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className="bg-red-100 text-6xl flex flex-col gap-2 align-center justify-center h-screen items-center">
+      Hola {process.env.name}
+      <Link href="/login">
+        Sign In
+      </Link>
+      <Link href="/register">
+        Register
+      </Link>
+    </div>
+  );
+}
